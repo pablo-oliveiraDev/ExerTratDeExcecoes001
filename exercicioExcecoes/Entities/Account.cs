6301987b19252bdab5206b0d5293ccb50bb84ff1
Pablo de Oliveira Ferreira <pabloliverfe@gmail.com>
2021-01-21 00:09:00 -0300

    correcao de nome dos metodos

--- exercicioExcecoes/Entities/Account.cs
+++ exercicioExcecoes/Entities/Account.cs
@@ -1,10 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+
 
 namespace exercicioExcecoes.Entities
 {
     class Account
     {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WhitdrawLimit { get; set; }
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance, double whitdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WhitdrawLimit = whitdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+             Balance += amount;
+        }
+        public void Whitdraw(double amount)
+        {
+            Balance -= amount;
+        }
     }
 }
