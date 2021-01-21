ad167d6c6fd0b6aac00bb4205c710dc5eeb2d991
Pablo de Oliveira Ferreira <pabloliverfe@gmail.com>
2021-01-20 23:58:37 -0300

    exercicio inciado e classe terminada

---
+++ exercicioExcecoes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicioExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
